27cb2fbb8a2158cbd888540a0668d17dd0ccc2e3
JConcept <jclepot1@hotmail.com>
2017-09-25 13:22:05 +0200

    rendered dynamic title

--- dist/assets/php/template/htmlTemplateHeader.php
+++ dist/assets/php/template/htmlTemplateHeader.php
@@ -1,11 +1,9 @@
 <?php 
 	$path = $_SERVER['PHP_SELF']; /* @fail : faille de sécurité en attaque XSS ? */
-  	$file = basename ($path); 
-
-    $reg = '#^\..*$#';
-    $onestla = preg_replace($reg, '$2', $file);
-	echo $onestla; 
-    $title = "titre";
+	$file = basename ($path);
+    $reg = '#\.php$#';
+	$title = preg_replace($reg, '', $file);
+    $title[0] = strtoupper($title[0]);
 ?> 
 <!DOCTYPE html>
 <html lang="fr">
@@ -15,5 +13,5 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="./vendor/bootstrap-3.3.7-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="./assets/css/style.css">
-	<title>Nelly Haikal - <?php echo($title); ?></title><!-- @TODO : créer fonction : fGetTitle(); -->
+	<title><?php echo($title); ?> - Nelly Haikal</title><!-- @TODO : créer fonction : fGetTitle(); -->
 </head>
