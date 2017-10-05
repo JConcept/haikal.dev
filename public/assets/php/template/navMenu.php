4ea556fb5984d6c0940264f3eecf2a02815400e7
JConcept <jclepot1@hotmail.com>
2017-10-05 02:00:28 +0200

    has corrected minor problem in html

--- public/assets/php/template/navMenu.php
+++ public/assets/php/template/navMenu.php
@@ -2,38 +2,38 @@
     <div class="container-fluid">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">Menu<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-menu-hamburger"></span></a>
+			<div class="navbar-toggle" data-toggle="collapse" data-target="#side-navbar">
+				<span>Menu</span>
+				<div class="pull-right">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</div>
+			</div>
+			<!-- <a class="navbar-brand" href="#">Menu<span class="pull-right showopacity glyphicon glyphicon-menu-hamburger"></span></a> -->
 		</div>
 		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
+		<div class="collapse navbar-collapse" id="side-navbar">
 			<ul class="nav navbar-nav">
-				<li id="nav-index" class="active"><a href="./accueil.php">Accueil<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+				<li id="nav-index" class="active"><a href="./accueil.php">Accueil<span class="pull-right showopacity glyphicon glyphicon-home"></span></a></li>
 				<li id="nav-galerie" class="dropdown">
-					<a href="./galerie.php" class="dropdown-toggle" data-toggle="dropdown">Galerie <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-th-large"></span></a><!-- book -->
+					<a href="./galerie.php" class="dropdown-toggle" data-toggle="dropdown">Galerie <span class="caret"></span><span class="pull-right showopacity glyphicon glyphicon-th-large"></span></a><!-- book -->
 					<ul class="dropdown-menu forAnimate" role="menu">
-						<li id="nav-photos"><a href="./photos.php"><span style="font-size:16px;" class="pull-left hidden-xs showopacity glyphicon glyphicon-picture"></span>Photos</a></li><!--camera-->
-						<li class="divider"></li>
-						<li id="nav-"><a href="./videos.php"><span style="font-size:16px;" class="pull-left hidden-xs showopacity glyphicon glyphicon-film"></span>Vidéos</a></li>
-						<li class="divider"></li>
-						<li id="nav-peintures"><a href="./peintures.php"><span style="font-size:16px;" class="pull-left hidden-xs showopacity glyphicon glyphicon-blackboard"></span>Peintures</a></li>
+						<li id="nav-photos"><a href="./photos.php"><span class="pull-left showopacity glyphicon glyphicon-picture"></span>Photos</a></li><!--camera-->
+						<li id="nav-"><a href="./videos.php"><span class="pull-left showopacity glyphicon glyphicon-film"></span>Vidéos</a></li>
+						<li id="nav-peintures"><a href="./peintures.php"><span class="pull-left showopacity glyphicon glyphicon-blackboard"></span>Peintures</a></li>
 					</ul>
 				</li>
-				<li id="nav-exposition"><a href="./expositions.php">Expositions<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-blackboard"></span></a></li>
-				<li id="nav-biographie"><a href="./biographie.php">Biographie<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-education"></span></a></li>
-				<!--<li id="nav-"><a href="#">Articles<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-text-background"></span></a></li>
-				<li id="nav-"><a href="#">Ateliers<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-th-list"></span></a></li>
-				--><li id="nav-contact"><a href="./contact.php">Contact<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
-				<!--<li id="nav-"><a href="#">Liens<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-globe"></span></a></li>
-				<li id="nav-"><a href="#">Presse<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-book">--><!--list-alt--><!--</span></a></li>
-				<li id="nav-"><a href="#">Partager<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-share"></span></a></li>-->
+				<li id="nav-exposition"><a href="./expositions.php">Expositions<span class="pull-right showopacity glyphicon glyphicon-blackboard"></span></a></li>
+				<li id="nav-biographie"><a href="./biographie.php">Biographie<span class="pull-right showopacity glyphicon glyphicon-education"></span></a></li>
+				<!--<li id="nav-"><a href="#">Articles<span class="pull-right showopacity glyphicon glyphicon-text-background"></span></a></li>
+				<li id="nav-"><a href="#">Ateliers<span class="pull-right showopacity glyphicon glyphicon-th-list"></span></a></li>
+				--><li id="nav-contact"><a href="./contact.php">Contact<span class="pull-right showopacity glyphicon glyphicon-envelope"></span></a></li>
+				<!--<li id="nav-"><a href="#">Liens<span class="pull-right showopacity glyphicon glyphicon-globe"></span></a></li>
+				<li id="nav-"><a href="#">Presse<span class="pull-right showopacity glyphicon glyphicon-book">--><!--list-alt--><!--</span></a></li>
+				<li id="nav-"><a href="#">Partager<span class="pull-right showopacity glyphicon glyphicon-share"></span></a></li>-->
 				<!--<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span class="pull-right showopacity glyphicon glyphicon-cog"></span></a>
 					<ul class="dropdown-menu forAnimate" role="menu">
 						<li><a href="#">Action</a></li>
 						<li><a href="#">Another action</a></li>
