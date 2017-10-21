cdd94d7a356d85b450311a05a912076df85ef3a6
JConcept <jclepot1@hotmail.com>
2017-10-22 00:07:34 +0200

    created DB migration with php artisan/laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_permission_id')->unsigned();
+            $table->foreign('user_permission_id')->references('id')->on('users_permissions');
+            $table->string('url', 100);
+            $table->string('last_name', 60);
+            $table->string('first_name', 100);
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('login', 100)->unique();
             $table->string('password');
+            $table->boolean('actif');
             $table->rememberToken();
             $table->timestamps();
         });
