7175747945a0b5e9f590a7f50c9339c87cfa9598
Jean-Christophe Lepot <github@jconcept.be>
2019-01-28 14:07:05 +0100

    ajout du favicon

--- resources/views/partials/htmlTemplateHeader.blade.php
+++ resources/views/partials/htmlTemplateHeader.blade.php
@@ -6,4 +6,5 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="{{ url('css/app.css') }}">
 	<title>{{$title}} - Nelly Haikal</title>
+	<link rel="icon" type="image/png" href="/img/favicon.png">
 </head>
